c8ea17186bf686f09355c905692fc75b9c779db2
nolongeralanguagemodel <thivyanth2004@gmail.com>
2024-02-03 18:42:34 +0530

    every 5 secs

--- background.js
+++ background.js
@@ -1,17 +1,19 @@
 function checkInternet() {
-    fetch('https://www.google.com', { mode: 'no-cors' })
-    .then(() => {
-        console.log('Internet is connected');
-    })
-    .catch(() => {
-        console.log('Internet disconnected, navigating to login page');
-        chrome.tabs.create({url: 'https://internet.iitb.ac.in/login.php'});
-    });
+  fetch('https://www.google.com', { mode: 'no-cors' })
+  .then(() => {
+      console.log('Internet is connected');
+  })
+  .catch(() => {
+      console.log('Internet disconnected, navigating to login page');
+      chrome.tabs.create({url: 'https://internet.iitb.ac.in/login.php'});
+  });
 }
 
-chrome.alarms.create('checkInternet', { periodInMinutes: 1 });
+// Set up an alarm to check internet connectivity every 5 seconds
+chrome.alarms.create('checkInternet', { periodInMinutes: 1 / 12 });
+
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'checkInternet') {
-    checkInternet();
-  }
+if (alarm.name === 'checkInternet') {
+  checkInternet();
+}
 });
