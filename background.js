24cd6f8d304af4240e7927a06af593b779287cb1
nolongeralanguagemodel <thivyanth2004@gmail.com>
2024-02-03 19:07:36 +0530

    detects user

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 // background.js
+let extensionOpenedTabId = null;
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.action === "loggedIn" && sender.tab) {
       console.log('Logged in successfully, closing tab.');
@@ -24,6 +25,19 @@ function checkInternet() {
   })
   .catch(() => {
       console.log('Internet disconnected, navigating to login page');
+      chrome.tabs.create({url: 'https://internet.iitb.ac.in/login.php'}, (tab) => {
+          extensionOpenedTabId = tab.id; // Store the ID of the tab opened by the extension
+      });
+  });
+}
+
+function checkInternet() {
+  fetch('https://www.google.com', { mode: 'no-cors' })
+  .then(() => {
+      // No need to check if the login tab is open, as the content script handles it
+  })
+  .catch(() => {
+      console.log('Internet disconnected, navigating to login page');
       chrome.tabs.create({url: 'https://internet.iitb.ac.in/login.php'});
   });
 }
@@ -33,5 +47,15 @@ chrome.alarms.create('checkUniversityNetwork', { periodInMinutes: 1/12 });
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'checkUniversityNetwork') {
     checkUniversityNetwork();
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action === "loggedIn" && sender.tab) {
+      if (sender.tab.id === extensionOpenedTabId) {
+          console.log('Logged in successfully, closing tab.');
+          chrome.tabs.remove(sender.tab.id);
+          extensionOpenedTabId = null; // Reset the variable as the tab is now closed
+      }
   }
 });
